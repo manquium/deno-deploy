41042ab70eeaa6ee9930b0d6e0ab50f34aafb2fb
manquium <mail@henningwilhelm.de>
2022-10-27 19:42:29 -0500

    ...

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
-serve((_req) => {
-  return new Response("Hello World!", {
-    headers: { "content-type": "text/plain" },
-  });
-});
+
+function Handler(_req){
+    return new Response("Hello World! 23", {
+        headers: { "content-type": "text/plain" },
+      });
+}
+serve(Handler);
