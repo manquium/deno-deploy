e1c550c64dd6d34d542948d0e3b1b10ad423cfe4
manquium <mail@henningwilhelm.de>
2022-10-27 20:07:22 -0500

    oak...jsx

--- index.ts
+++ index.ts
@@ -1,13 +1,20 @@
 import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import {page} from "./page.tsx";
 
 const router = new Router();
 router.get("/", (ctx) => {
   ctx.response.body = "Hello world!";
 });
 
+router.get("/hello", (ctx) => {
+  ctx.response.body = page({});
+});
+
 router.get("/hello/:name", (ctx) => {
     ctx.response.body = `Hello ${ctx.params.name?ctx.params.name:"Peter"}!`;
 });
+
+
   
 
 const app = new Application();
